24af6ef6afbb994f87298b4c9ca77754ca048082
Manuel Werder <manuel@konzeptplus.ch>
2021-02-13 22:33:13 +0100

    Chapter 1, Exercise 1-5

--- ansi_c/chapter_1/exe_1-5/fahrtoceltab.c
+++ ansi_c/chapter_1/exe_1-5/fahrtoceltab.c
@@ -10,4 +10,5 @@ int main(void) {
     printf("Shows a Fahrenheit to Celsius table.\n");
     for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
         printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+    return 0;
 }
